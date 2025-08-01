e0f2b86657913d0abcd774c6d3c4da8ae229a942
Veikka Hakulinen <2205790@edu.karelia.fi>
2025-08-01 13:01:40 +0300

    test

--- logout.php
+++ logout.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 require_once __DIR__ . '/config.php';
 session_destroy();
 header("Location: login.php");
--- sendEmail.php
+++ sendEmail.php
@@ -1,4 +1,4 @@
-﻿<?php
+<?php
 require_once __DIR__ . '/config.php';
 if (!isset($_SESSION["user"])) {
     header("Location: login.php");
